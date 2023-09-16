de2c192cab2cdfecd9eb9197dc163df384da3d92
DmytroBrekalo <brekalo4@gmail.com>
2023-09-16 14:45:56 +0300

    loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,15 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import style from './App.module.css';
-import { useSelector } from 'react-redux';
-import { getContacts, getIsLoading } from 'redux/contacts/contactsSelector';
+
 export const App = () => {
-  const isLoading = useSelector(getIsLoading);
-  const contacts = useSelector(getContacts);
   return (
     <div className={style.main_section}>
       <h1>Phonebook</h1>
       <ContactForm />
-
       <h2>Contacts</h2>
       <Filter />
-      {isLoading && <p>Loading...</p>}
-      {contacts && !isLoading && <ContactList />}
+      <ContactList />
     </div>
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 import style from './ContactList.module.css';
 import { deleteContact } from 'redux/contacts/contactsOperations';
-import { getContacts } from 'redux/contacts/contactsSelector';
+import { getContacts, getIsLoading } from 'redux/contacts/contactsSelector';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
 
   const contacts = useSelector(getContacts);
   const stateFilter = useSelector(state => state.filter);
-
+  const isLoading = useSelector(getIsLoading);
   const getFilteredPerson = () => {
     const normalisedFilter = stateFilter.toLowerCase();
 
@@ -17,15 +17,22 @@ export const ContactList = () => {
     );
   };
   return (
-    <ul className={style.list}>
-      {getFilteredPerson().map(({ id, name, phone }) => {
-        return (
-          <li key={id} id={id} className={style.list_item}>
-            {name}: {phone}
-            <button onClick={() => dispatch(deleteContact(id))}>Delete</button>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      {isLoading && <p>Loading...</p>}
+      {contacts && !isLoading && (
+        <ul className={style.list}>
+          {getFilteredPerson().map(({ id, name, phone }) => {
+            return (
+              <li key={id} id={id} className={style.list_item}>
+                {name}: {phone}
+                <button onClick={() => dispatch(deleteContact(id))}>
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
   );
 };
